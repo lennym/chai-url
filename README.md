861ede00f08a9e77149f43ddcfeee8af92b2c50c
Leonard Martin <leonard.martin@gmail.com>
2017-03-01 10:18:15 +0000

    Update docs and tests

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # chai-url
 A chai assertion plugin for working with urls
+
+Provides a number of assertion helpers for working with urls.
+
+## Usage
+
+```js
+const chai = require('chai');
+chai.use(require('chai-url'));
+
+chai.expect(http://example.com/foo/bar).to.have.path('/foo/bar');
+chai.expect(http://example.com/foo/bar).to.have.protocol('http');
+```
+
+## Available matchers
+
+* `path`
+* `pathname`
+* `port`
+* `hostname`
+* `protocol`
+* `auth`
+* `hash`
+
+In each case, the property is tested against the corresponding property from node's [url.parse](https://nodejs.org/api/url.html#url_url_strings_and_url_objects) method.
+
+## Partial matching
+
+The `path`, `pathname`, `hostname`, `auth` and `hash` functions can also perform partial matching based on substrings by using a `contains` clause in the test statement.
+
+```js
+expect('http://example.com/foo/bar').to.contain.path('/foo');
+```
--- test/index.js
+++ test/index.js
@@ -37,6 +37,74 @@ describe('chai-url', () => {
       expect('http://www.example.com/foo/bar').to.contain.hostname('example.com');
     });
 
+    it('handles domain-less urls', () => {
+      expect('/foo/bar').not.to.have.hostname();
+    });
+
+  });
+
+  describe('protocol', () => {
+
+    it('matches protocol', () => {
+      expect('http://example.com/foo/bar').to.have.protocol('http');
+    });
+
+    it('doesn\'t matter if expected protocol has trailing : or not', () => {
+      expect('http://example.com/foo/bar').to.have.protocol('http');
+      expect('http://example.com/foo/bar').to.have.protocol('http:');
+    });
+
+    it('handles protocol-less urls', () => {
+      expect('//example.com/foo/bar').not.to.have.protocol();
+    });
+
+  });
+
+  describe('auth', () => {
+
+    it('matches auth segment of urls', () => {
+      expect('http://user:pass@example.com/foo/bar').to.have.auth('user:pass');
+    });
+
+    it('handles auth-less urls', () => {
+      expect('http://example.com/foo/bar').not.to.have.auth();
+    });
+
+  });
+
+  describe('hash', () => {
+
+    it('matches hash segment of urls', () => {
+      expect('http://example.com/foo/bar#section').to.have.hash('section');
+      expect('#section').to.have.hash('section');
+    });
+
+    it('ignores leading # symbol', () => {
+      expect('http://example.com/foo/bar#section').to.have.hash('section');
+      expect('http://example.com/foo/bar#section').to.have.hash('#section');
+    });
+
+    it('handles hash-less urls', () => {
+      expect('http://example.com/foo/bar').not.to.have.hash();
+    });
+
+  });
+
+  describe('port', () => {
+
+    it('matches port segment of urls', () => {
+      expect('http://example.com:3000/').to.have.port(3000);
+    });
+
+    it('handles both string and numerical values', () => {
+      expect('http://example.com:3000/foo/bar').to.have.port(3000);
+      expect('http://example.com:3000/foo/bar').to.have.port('3000');
+    });
+
+    it('handles port-less urls', () => {
+      expect('http://example.com/foo/bar').not.to.have.port();
+    });
+
   });
 
 });
